7df96ca442ff08e31d7c22afbd4c1af2a8489110
Andrey <antonov.andrey.10101@gmail.com>
2023-09-13 19:44:00 +0300

    feat: change styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>Hello</div>
+  
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
-  setup() {
-    return {};
+  name: "App",
+  components: {
+    HeroImage,
+    Text,
   },
 };
 </script>
 
-<style scoped>
+<style>
+#app {
+  min-width: 18.75rem;
+  max-width: 45.625rem;
+  width: 100%;
+  height: fit-content;
+  margin: 0rem 1.5rem;
+  background-color: hsl(0, 0%, 100%);
+  box-shadow: 0rem 2.5rem 2.5rem -0.625rem rgba(201, 213, 225, 0.503415);
+  border-radius: 0.625rem;
+}
+
+@media screen and (min-width: 600px) {
+  #app {
+    display: flex;
+  }
+}
 </style>
